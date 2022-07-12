83ba278d11698b4bc65ffdf013c74248e2b68c64
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-07-12 17:08:22 +0530

    admin routes

---
+++ routes/admin.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const router=express.Router();
+const adminController=require('../controllers/admin');
+
+router.get('/products',adminController.getProducts);
+
+module.exports=router;
