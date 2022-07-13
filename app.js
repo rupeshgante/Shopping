fb09f72e97b695b52c8d1b8bc9a439e17af03999
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-07-13 20:04:32 +0530

    added assosiations

--- app.js
+++ app.js
@@ -1,24 +1,60 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const cors=require('cors');
 
+app.use(cors());
+app.use(bodyParser.json({ extended: false }));
 
-const app = express();
+
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart=require('./models/cart');
+const CartItem=require('./models/cart-item');
 
 const adminRoutes = require('./routes/admin');
 
-app.use(cors());
-app.use(bodyParser.json({ extended: false }));
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use(adminRoutes);
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through:CartItem});
+Product.belongsToMany(Cart,{through:CartItem});
 
 sequelize
   // .sync({ force: true })
   .sync()
   .then(result => {
-    // return User.findByPk(1);
-    console.log(result);
-    app.listen('5000');
-  }).catch(err=>console.log(err));  
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'rupesh', email: 'rupesh@gmail.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+   return user.createCart();
+  })
+  .then(cart=>{
+    app.listen(5000);
 
+  })
+  .catch(err => {
+    console.log(err);
+  });
