f4e1eec60c5cc0e7cb64a1ba72e81ef1b4b71679
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-07-20 12:29:56 +0530

    added order routres

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,16 @@ const express=require('express');
 const router=express.Router();
 const adminController=require('../controllers/admin');
 
-router.get('/products',adminController.getProducts);
+router.get('/',adminController.getIndex);
+
+router.get('/products/:page',adminController.getIndex);
+
+router.get('/cart', adminController.getCart);
+
+router.post('/cart', adminController.postCart);
+
+router.post('/orders',adminController.postOrder);
+
+router.get('/orders',adminController.getOrder);
 
 module.exports=router;
