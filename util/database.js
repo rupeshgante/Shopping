8567111b884fe9c8bd50c6c58866fc77ff85dbdd
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-07-24 22:33:20 +0530

    database route

--- util/database.js
+++ util/database.js
@@ -1,8 +1,12 @@
 const {Sequelize} = require('sequelize');
+const dotenv=require('dotenv');
+dotenv.config();
 
-const sequelize = new Sequelize('e-commerce', 'root', 'Learn@1122', {
+console.log(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD);
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   dialect: 'mysql',
-  host: 'localhost'
+  host: process.env.DB_HOST
 });
 
 module.exports = sequelize;
