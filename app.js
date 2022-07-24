794f5290b213b0e04780cb48fe61eef2a162a272
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-07-24 22:33:38 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors=require('cors');
+const dotenv=require('dotenv');
+dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json({ extended: false }));
